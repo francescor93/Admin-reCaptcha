be96d5209d92d03f56c521f747dcf02448848c98
Francesco <35368854+francescor93@users.noreply.github.com>
2018-10-18 13:02:28 +0200

    Update plugin.php
    Edited old content. Changed reCaptcha siteverify to use cURL to send verification request and reCaptcha Javascript code to use Invisible widget.

--- plugin.php
+++ plugin.php
@@ -1,109 +1,151 @@
 <?php
 /*
-
-!!! CONTENT UNDER CONSTRUCTION !!!
-
-Plugin Name: Admin reCaptcha
-Plugin URI: https://github.com/armujahid/Admin-reCaptcha.git
-Description: This plugin enable reCapcha on Admin login screen
-Version: 1.2
-Author: Abdul Rauf
-Author URI: http://armujahid.me/
+Plugin Name: Admin reCaptcha Invisible
+Plugin URI: https://github.com/francescor93/Admin-reCaptcha.git
+Description: New version of armujahid's "Admin reCaptcha". It uses reCaptcha V2 Invisible and cURL to send request.
+Version: 1.0
+Author: Francesco Rega
+Author URI: https://www.francescorega.eu
 */
 
-if( !defined( 'YOURLS_ABSPATH' ) ) die();
+// Die if not requiring from YOURLS
+if (!defined('YOURLS_ABSPATH')) { die; exit; }
 
-yourls_add_action( 'pre_login_username_password', 'abdulrauf_adminreCaptcha_validatereCaptcha' );
+// Add custom action to pre login
+yourls_add_action('pre_login_username_password', 'ReFraReCaptcha_validate');
 
-// Validates reCaptcha
-function abdulrauf_adminreCaptcha_validatereCaptcha()
-{
-	include('captcha.php'); 
-	if ($resp != null && $resp->success) 
-	{ 
-		//reCaptcha validated
-		return true;
+// Function to check if reCaptcha is valid
+function ReFraReCaptcha_validate() {
+
+	// Get private key
+	$privKey = yourls_get_option('ReFraReCaptcha_privKey');
+
+	// Prepare cURL request
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$params = http_build_query(array(
+		'secret' => $privKey, 
+		'response' => $_POST['g-recaptcha-response'],
+		'remoteip' => $_SERVER['REMOTE_ADDR']
+	));	
+	$request = curl_init();
+	curl_setopt($request, CURLOPT_URL, $url);
+	curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($request, CURLOPT_POST, true);
+	curl_setopt($request, CURLOPT_POSTFIELDS, $params);
+	
+	// Send cURL request
+	$response = curl_exec($request);
+	
+	// If response is available
+	if ($response) {
+		
+		// Decode JSON
+		$json = json_decode($response);
+		
+		// If having "success" key return true
+		if ($json->success) {
+			return true;
+		}
+		
+		// Else return false and terminates
+		else {
+			yourls_do_action('login_failed');
+			yourls_login_screen($error_msg = 'reCaptcha validation failed');
+			exit;
+			return false;
+		}
 	}
-	else
-	{
-		yourls_do_action( 'login_failed' );
-		yourls_login_screen( $error_msg = 'reCaptcha validation failed' );
-		die();
+	else {
+		yourls_do_action('login_failed');
+		yourls_login_screen($error_msg = 'reCaptcha validation failed');
+		exit;
 		return false;
 	}
 }
 
-// Register plugin on admin page
-yourls_add_action( 'plugins_loaded', 'abdulrauf_adminreCaptcha_init' );
-function abdulrauf_adminreCaptcha_init() {
-    yourls_register_plugin_page( 'adminreCaptcha', 'Admin reCaptcha Settings', 'adminreCaptcha_config_page' );
+// Add custom action after loading plugins
+yourls_add_action('plugins_loaded', 'ReFraReCaptcha_init');
+
+// Register plugin page in administrative area
+function ReFraReCaptcha_init() {
+    yourls_register_plugin_page('adminReCaptchaInvisible', 'Admin reCaptcha Invisible Settings', 'ReFraReCaptcha_config' );
 }
 
-// The function that will draw the config page
-function adminreCaptcha_config_page() {
-    	 if( isset( $_POST['abdulrauf_adminreCaptcha_public_key'] ) ) {
-	        yourls_verify_nonce( 'abdulrauf_adminreCaptcha_nonce' );
-	        abdulrauf_adminreCaptcha_save_admin();
-	    }
+// Define plugin page
+function ReFraReCaptcha_config() {
+
+	// After clicking Save
+	if (isset($_POST['ReFraReCaptcha_save'])) {
+			
+		// Verify nonce
+		yourls_verify_nonce( 'ReFraReCaptcha_nonce' );
+		
+		// Get values from form
+		$pubkey = $_POST['ReFraReCaptcha_publicKey'];
+		$privkey = $_POST['ReFraReCaptcha_privateKey'];
+		
+		// If public key option already exists update it, else add it
+		if (yourls_get_option('ReFraReCaptcha_pubKey') !== false) {
+			yourls_update_option('ReFraReCaptcha_pubKey', $pubkey);
+		} 
+		else {
+			yourls_add_option('ReFraReCaptcha_pubKey', $pubkey);
+		}
+		
+		// If private key option already exists update it, else add it
+		if (yourls_get_option('ReFraReCaptcha_privKey') !== false) {
+			yourls_update_option('ReFraReCaptcha_privKey', $privkey);
+		} 
+		else {
+			yourls_add_option('ReFraReCaptcha_privKey', $privkey);
+		}
+		
+		// Show confirm message
+		echo "Saved";
+	}
     
-    $nonce = yourls_create_nonce( 'abdulrauf_adminreCaptcha_nonce' );
-    $pubkey = yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key', "" );
-    $privkey = yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key', "" );
-    echo '<h2>Admin reCaptcha plugin settings</h2>';
-    echo '<form method="post">';
-    echo '<input type="hidden" name="nonce" value="' . $nonce . '" />';
-    echo '<p><label for="abdulrauf_adminreCaptcha_public_key">reCaptcha site key: </label>';
-    echo '<input type="text" id="abdulrauf_adminreCaptcha_public_key" name="abdulrauf_adminreCaptcha_public_key" value="' . $pubkey . '"></p>';  
-    echo '<p><label for="abdulrauf_adminreCaptcha_private_key">reCaptcha secret key: </label>';
-    echo '<input type="text" id="abdulrauf_adminreCaptcha_private_key" name="abdulrauf_adminreCaptcha_private_key" value="' . $privkey . '"></p>';
-    echo '<input type="submit" value="Save"/>';
-    echo '</form>';
-
+	// Get form values
+    $nonce = yourls_create_nonce('ReFraReCaptcha_nonce');
+    $pubkey = yourls_get_option('ReFraReCaptcha_pubKey');
+    $privkey = yourls_get_option('ReFraReCaptcha_privKey', "" );
+	
+	// Show form
+    echo '
+	<h2>Admin reCaptcha Invisible plugin settings</h2>
+	<form method="post">
+		<input type="hidden" name="nonce" value="' . $nonce . '">
+		<p>
+			<label for="ReFraReCaptcha_publicKey">reCaptcha site key: </label>
+			<input type="text" id="ReFraReCaptcha_publicKey" name="ReFraReCaptcha_publicKey" value="' . $pubkey . '">
+		</p>
+		<p>
+			<label for="ReFraReCaptcha_privateKey">reCaptcha secret key: </label>
+			<input type="text" id="ReFraReCaptcha_privateKey" name="ReFraReCaptcha_privateKey" value="' . $privkey . '">
+		</p>
+		<input type="submit" name="ReFraReCaptcha_save" value="Save">
+	</form>';
 }
 
-// Save reCaptcha keys in database 
-function abdulrauf_adminreCaptcha_save_admin()
-{
-	$pubkey = $_POST['abdulrauf_adminreCaptcha_public_key'];
-	$privkey = $_POST['abdulrauf_adminreCaptcha_private_key'];
-	if ( yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' ) !== false ) {
-        yourls_update_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
-    } 
-	else {
-        yourls_add_option( 'abdulrauf_adminreCaptcha_pub_key', $pubkey );
-    }
-	if ( yourls_get_option( 'abdulrauf_adminreCaptcha_priv_key' ) !== false ) {
-        yourls_update_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
-    } 
-	else {
-        yourls_add_option( 'abdulrauf_adminreCaptcha_priv_key', $privkey );
-    }
-    echo "Saved";
-}
+// Add custom action in html head tag
+yourls_add_action('html_head', 'ReFraReCaptcha_addJS');
 
 // Add the JavaScript for reCaptcha widget
-yourls_add_action( 'html_head', 'abdulrauf_adminreCaptcha_addjs' );
-function abdulrauf_adminreCaptcha_addjs() {
-	$siteKey = yourls_get_option( 'abdulrauf_adminreCaptcha_pub_key' );
+function ReFraReCaptcha_addJS() {
 	?>
-	<script type="text/javascript">
-	//JQuery function to add div for reCaptcha widget and load js only on login screen
-	$(document).ready(function() {
-		var logindiv = document.getElementById('login');
-		if (logindiv != null) { //check if we are on login screen
-			//getting reCaptcha script by jquery only on login screen
-			$.getScript( "https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit");
-			var form = logindiv.innerHTML;
-			var index = form.indexOf('<p style="text-align: right;">'); //finding tag before which reCaptcha widget should appear
-			document.getElementById('login').innerHTML = form.slice(0, index) + '<div id="captcha_container"></div>' + form.slice(index);	    
+	<script>
+		function sendForm() {
+			$("#login form").submit();
 		}
-    });
-	// JavaScript function to explicitly render the reCAPTCHA widget
-	var loadCaptcha = function() {
-	  captchaContainer = grecaptcha.render('captcha_container', {
-		'sitekey' : '<?php echo $siteKey?>'
-	  });
-	};
+		$(document).ready(function() {
+			if ($("#login").length) {
+				$("#submit").addClass("g-recaptcha");
+				$("#submit").attr('data-sitekey','<?php echo yourls_get_option('ReFraReCaptcha_pubKey'); ?>');
+				$("#submit").attr('data-callback','sendForm');
+				$("#submit").attr('name','btnsubmit');
+				$("#submit").attr('id','btnsubmit');
+				$.getScript("https://www.google.com/recaptcha/api.js");
+			}
+		});
 	</script>
 	<?php
 }
